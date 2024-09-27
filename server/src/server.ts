a49f2b66412ac66d01cddfbfb63529acc7c015c0
Justin Kao <justinkao010@gmail.com>
2024-09-26 22:22:01 -0700

    asdf

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 // import { fileURLToPath } from 'node:url';
 // import path from 'path';
-import seedAll from './seeds/index.js';
+// import seedAll from './seeds/index.js';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
@@ -29,7 +29,7 @@ app.use(routes);
 //   });
 // }
 
-seedAll();
+// seedAll();
 
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
